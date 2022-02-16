dc87b54d363374a45399408e2bb78849217a06e0
Therman116220 <thermanhaynes@gmail.com>
2022-02-16 08:56:14 -0600

    update

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,92 @@
-/*
-public class HelloWorld {
+//Defines a object  "A.E. Object Oriented
+public class HelloWorld {    /*
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
 */
 
+/*
 //Bonus
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("java Hello World");
     }
+}
+*///This p.s.void.main is called "The main method"
+// argument expected is a string (args
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+//1 ✅
+        int myFavoriteNumber = 72;
+    /*    System.out.println(myFavoriteNumber + " is my favorite number");*/
+
+//2 ✅
+
+        /*String myString = "Create a string like this";
+        System.out.println(myString);*/
+//3 ✅
+    /*    char myString = 'C';                     //Example of Incorrect way i guess
+        System.out.println(myString);*/
+//4 ✅
+   /* float myString;
+    myString = (float) 3.14159;
+    System.out.println(myString);*/
+//5 ✅  //corriculumn asked me not to fill the var(for future me)
+/*long myNumber;
+*//*System.out.println(myNumber);*/
+//6                   //shpuld maybe be float
+/*double myNumber = 3.14;
+System.out.println(myNumber);*/
+//7
+/*myNumber = (double) 123L;*/
+
+//8
+         /*myNumber = 3.14;*/
+
+//9
+
+
+//10
+//Increments 5-6
+        /*int x = 5;
+        System.out.println(x++);
+        System.out.println(x);*/
+//Increments 6-6, based of the placement of the incrementation it will loop add it first.
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//11
+
+
+//12
+
+
+
+
+
+
+
+
+
+
+    }
+// javac HelloWorld.java     ---seems to save
+// java HelloWorld           ---seems to populate main method
+
+//METHOD -similar to function in javascript
+// main method -> specific method in java that acts as our entry point when running java
+// (similar to "Starting point:GO" in board game, or front door of house, you need to walk
+// through to access other rooms or ANYTHING)
+
+
+
+
+
+
+
+
 }
