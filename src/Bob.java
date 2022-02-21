c4433b2c67071e43ba8a72e38b07c603e9907d53
Therman116220 <thermanhaynes@gmail.com>
2022-02-20 19:21:55 -0600

    update

--- src/Bob.java
+++ src/Bob.java
@@ -5,11 +5,12 @@ public class Bob {
 
         Scanner BossBob = new Scanner(System.in);
         String Talking;
-
+        System.out.println("Talk to me");
             Talking = BossBob.nextLine();
             if (Talking.contains("?")) {
                 System.out.println("Sure thing broski");
             } else if (Talking.endsWith("!")) {
+
                 System.out.println("Take it down a few notches friend..");
             } else if (Talking.equals("")) {
                 System.out.println("Fine, We do not need to talk. Just hang");
--- src/ControlFlowExercise.java
+++ src/ControlFlowExercise.java
@@ -3,11 +3,50 @@ import java.util.Scanner;
 public class ControlFlowExercise {
 
     public static void main(String[] args) {
-        int i = 5;
+   //1✅
+     /*   // int i = 5;
         while (i <= 15) {
             i++;
         }
-        ;
+        ;*/
+
+//b✅
+ //Part one
+   /* int count = 0;*/
+
+
+   /* do {
+        System.out.println("Count: " + count);
+      count+= 2;
+    } while (count<=100);*/
+//Part Two
+        int count = 100;
+        do {
+            System.out.println("Count: " + count);
+            count-= 5;
+        } while (count>=0);
+
+
+//Part Three
+
+        double counter = 2;
+        do {
+            System.out.println("Count: " + counter);
+            counter = counter * counter;
+        } while (counter<=1000000);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
