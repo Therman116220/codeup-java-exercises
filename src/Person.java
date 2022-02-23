787d811d601615681252b1582f9afed2b8443e6b
Therman116220 <thermanhaynes@gmail.com>
2022-02-23 10:14:29 -0600

    update

---
+++ src/Person.java
@@ -0,0 +1,80 @@
+/*
+import javax.xml.namespace.QName;
+
+public class Person {
+    public static void main(String[] args) {
+
+    }
+
+
+    private String name;
+    private int age;
+    private static String relationship;
+
+    public Person(name, age, relationship) {
+        this.name = name;
+        this.age = age;
+        this.relationship = relationship;
+
+    }
+
+    public String getName(String name){
+//TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name){
+//TODO: change the name field to the passed value
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+    }
+
+
+
+
+
+
+*/
+/*    public String firstName;
+    public String lastName;
+    //Extra examples: More fields toidentify
+
+    private int age;
+    public String relationship;
+
+    //Properties can be methods
+
+    public String sayHello() {
+        return String.format("Hello from %s %s!", firstName, lastName);
+    }
+
+    public static void main(String[] args) {
+        //Create an Instance of a person:
+        Person Therman = new Person();
+        //Filled on properties with values:
+        Therman.firstName = "Therman";
+        Therman.lastName = "Haynes";
+        Therman.age = 24;
+        Therman.relationship = "taken";
+
+        System.out.println("Therman.firstname = " + Therman.firstName);
+        System.out.println("Therman.lastName = " + Therman.lastName);
+        System.out.println("Therman.age = " + Therman.age);
+        System.out.println("Therman.relationship = " + Therman.relationship);
+    // Adding in new line of white space:
+
+   Person cody = new Person();
+
+   cody.firstName = "Cody";
+   cody.lastName = "Hodges";
+   cody.age = 32;
+    }*//*
+
+
+}
+
+
+
+
+*/
