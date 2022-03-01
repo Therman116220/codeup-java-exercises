cb3406bda3eb2925ce6fd9fb22799a806d665e8f
Therman116220 <thermanhaynes@gmail.com>
2022-03-01 08:59:26 -0600

    update

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -64,8 +64,94 @@ public class ArraysExercises {
         // [[10,000],[12,000],[20,000]]
 
 
+        // HOW TO ITERATE THROUGH ARRAYS
+
+        // //TODO: 👩🏼‍💻 Let's create a method (sumAll) that returns the sum of all integers in an int array (nums)
+        // example: array of ints:[5, 2, 3, 1] // returns: 11
+
+        // // // PSEUDOCODE
+        // 1. create a method (nums)
+public static int sumAll() {
 
 
+
+
+        };
+        // // what's the method signature? --> public static
+        // // what data type (if any) is being returned from this method --> int
+        //
+        // 2. create an array of ints
+        // // 2a. What's the length of our array? --> (5)
+        // // 2b. Where does this array go? --> in sumAll method
+int[] nums = {1,2,3,4,5};
+        // 3. Assign values to the array
+        // // how are we going to initialize the arrat of ints?
+
+        // // // array literal {}
+        // // // array declaration new int[]
+
+        // 4. iterate through our elements in the array
+        // // where does the iteration live? --> inside of the sumAll()
+        // // how do you want to iterate?
+        // // // traditional FOR loop
+        // // // ** enhanced for
+                int sum = 0;
+        // 5. create a variable to store the sum
+        // // where does this variable live? --> in sumAll() --> before (outside)
+                for (int n : nums) {
+                    sum += n;
+                    // // what's this variable's type (int)
+                    // // what's this variable's starting point?
+
+                    // 6. add logic that will add each element to itself?
+                    // where does this logic live?
+                    // int sum;
+                    // // [1,5,3,2]
+                    //  //  ** sum variable = sum + index of element **
+                    // // 1st iteration: sum = 0 -> sum = 0 + 1 // sum = 1;
+                    // // 2nd iteration: sum = 1 -> sum = 1 + 5 // sum = 6;
+
+                    // 7. return the sum;
+                    // // where does this return statement live? --> in sumAll() after (outside) loop
+
+                    // 8. call the method
+                    // // where and how do we call the method --> in PSVM
+                    // // sout the result of the sumAll method
+                    System.out.println(sumAll());
+                }
+    }
+
+//1
+
+
+/*    int[] numbers = {1, 2, 3, 4, 5};
+System.out.println(numbers);*/
+ //2
+public class Person {
+/* public static void main(String[] args) {
+
+    }*/
+
+
+    public String name;
+    private int age;
+    public static String relationship;
+
+    public Person(name, age, relationship) {
+        this.name = name;
+        this.age = age;
+        this.relationship = relationship;
 
     }
+
+    class listOfPersons {
+        Person[] People = new Person[3];
+    }
+
 }
+
+
+
+}
+
+
