529bfbb18ccfbd7fd34fcbbcbeea85a36046b104
Therman116220 <thermanhaynes@gmail.com>
2022-03-03 08:59:07 -0600

    update

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,7 +1,35 @@
 package shapes;
 
-abstract class Quadrilateral {
 
+/*
+import java.awt.*;
+*/
 
+public abstract class Quadrilateral extends shape implements measurable {
 
+    protected int length;
+    protected int width;
 }
+
+
+public Quadrilateral(int length, int width) {
+    this.width =width;
+    this.length =length;
+}
+
+  public int grabLength() {
+return length;
+return width;
+  }
+
+  public static void main(String[] args) {
+        Measureable myShape = new rectangle();
+    }
+}
+public abstract void setWidth(int width) {
+      return  this.width = width;
+        }
+
+public abstract void setLength(int length) {
+    return this.length = length;
+        }
--- src/shapes/rectangle.java
+++ src/shapes/rectangle.java
@@ -6,10 +6,10 @@ public class rectangle extends Quadrilateral {
     public double length;
     public double width;
 
-    public rectangle(double sidesOfRectangle, int i) {
-    }
+  /*  public rectangle(double sidesOfRectangle, int i) {
+    }*/
 
-    public double rectangle(double length, double width) {
+    public double rectangle(double sides) {
         this.length = length;
         this.width = width;
 
@@ -24,22 +24,22 @@ public class rectangle extends Quadrilateral {
 
 
 
-    public rectangle(double sidesOfRectangle) {
-        this.length = sidesOfRectangle;
-        this.width = sidesOfRectangle;
+
+
+
+    public  double perimeter() {
+        return (this.length * 2) + (this.width * 2);
     }
     public  double area() {
         return  this.length * this.width;
     }
-    public  double perimeter() {
-        return (this.length * 2) + (this.width * 2);
-    }
+
 
 
     public static void main(String[] args) {
 
        //Argument//
-        rectangle one = new rectangle(2,4);
+        rectangle one = new rectangle();
 /*        rectangle two = new rectangle(2,4);
         rectangle three = new rectangle(2,4);
         rectangle four = new rectangle(2,4);*/
