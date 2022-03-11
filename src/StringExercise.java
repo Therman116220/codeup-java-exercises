02744002524afe7c7d07101d8326d53e54d936b3
Therman116220 <thermanhaynes@gmail.com>
2022-03-11 09:11:09 -0600

    mistake

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,23 +1,11 @@
-
-import java.util.Scanner;
-
-
-public class StringExercise {
+import java.util.ArrayList;
+import java.util.List;
+public class Marker {
     public static void main(String[] args) {
-
-
-
-String pinkFloyd = "We don't need no ";
-        System.out.println(pinkFloyd + "education\n"  + pinkFloyd + "thought control");
-
-
-
-
-
-
-
-
-
-
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(13);
+        numbers.add(new Integer(8)); // Line 7
+        numbers.add(3.6); // Line 8
+        for (Integer number: numbers) System.out.println(number); // Line 9
     }
 }
