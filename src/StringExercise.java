5e0aa76a8f66dce9aca952f825129f527dc9eccd
Therman116220 <thermanhaynes@gmail.com>
2022-03-11 09:12:21 -0600

    fixed again

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,11 +1,17 @@
+
+import java.util.Scanner;
+
+
+public class StringExercise {
 import java.util.ArrayList;
 import java.util.List;
-public class Marker {
-    public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(13);
-        numbers.add(new Integer(8)); // Line 7
-        numbers.add(3.6); // Line 8
-        for (Integer number: numbers) System.out.println(number); // Line 9
+    public class Marker {
+        public static void main(String[] args) {
+            List<Integer> numbers = new ArrayList<>();
+            numbers.add(13);
+            numbers.add(new Integer(8)); // Line 7
+            numbers.add(3.6); // Line 8
+            for (Integer number: numbers) System.out.println(number); // Line 9
+        }
     }
 }
