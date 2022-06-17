21c1193905e275d802844c17c2c87e1f24751dc9
Therman116220 <thermanhaynes@gmail.com>
2022-06-17 01:26:53 -0500

    post grad repeated practice w/ objects

---
+++ src/src/Headphones.java
@@ -0,0 +1,39 @@
+public class Headphones {
+    public static void main(String[] args) {
+     Headphones h1 = new Headphones();
+        /*Properties*/
+
+        System.out.println(h1.color);
+        System.out.println(h1.porttype);
+        System.out.println(h1.brand);
+        System.out.println("$" + h1.price);
+        System.out.println(h1.color + " " + h1.brand +" headphones for $" + h1.price + "!");
+        /*Actions*/
+
+      /*  System.out.println(m.);*/
+
+    }
+/*Actions Declaration*/
+  String[] controls =  {
+            "volume",
+            "powerbtn",
+            "play/pause",
+            "skip",
+  };
+  /*Properties Declaration*/
+    String porttype = "micro-usb";
+    String color = "CandyGreen";
+    String brand = "SkullCandy";
+    Double price = 14.44;
+
+
+
+
+
+
+
+
+
+}
+
+
