2d72fe0213e8c4d94cb91ba1d5094b4f89557024
Therman116220 <thermanhaynes@gmail.com>
2022-06-17 00:12:38 -0500

    post grad notes/updates

--- src/src/StringExercise.java
+++ src/src/StringExercise.java
@@ -1,17 +1,17 @@
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class StringExercise {
-import java.util.ArrayList;
-import java.util.List;
+
     public class Marker {
         public static void main(String[] args) {
             List<Integer> numbers = new ArrayList<>();
             numbers.add(13);
-            numbers.add(new Integer(8)); // Line 7
-            numbers.add(3.6); // Line 8
-            for (Integer number: numbers) System.out.println(number); // Line 9
+            numbers.add(8);
+            numbers.add(3); // Line 8
+            for (Integer number: numbers) System.out.println(number);
         }
     }
 }
