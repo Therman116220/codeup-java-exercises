3b7649adadde6b990479a994a4ca3d001a92a0ed
Therman116220 <thermanhaynes@gmail.com>
2022-06-17 01:08:46 -0500

    post grad repeated practice w/ objects

---
+++ src/src/Pet.java
@@ -0,0 +1,17 @@
+public class Pet {
+    public static void main(String[] args) {
+        System.out.println(m.color);
+        System.out.println(m.type);
+        System.out.println(m.age);
+    }
+
+    String color = "beige";
+    String type = "Dog";
+
+
+
+
+
+}
+
+
