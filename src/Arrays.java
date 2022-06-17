7937e80753d92d1c3eca7bcdf2e29db2e170f5f9
Therman116220 <thermanhaynes@gmail.com>
2022-06-16 23:42:21 -0500

    post grad notes

--- src/Arrays.java
+++ src/Arrays.java
@@ -8,9 +8,17 @@ import java.util.Arrays;
 * Fun Reference- Like a address book, that starts w/  "0"
 *MUST DEFINE SIZE - If size will be consistant, and will not change during run-time.
 *
-*
-*
-* */
+**/
+
+
+
+
+
+/*Bounds of Array*/
+/*Bounds are the size*/
+/*Out of bounds is requestig data from out of index of the array (ex. Asking for 11 with a bounds of 0-10)
+*Zero-base index system: Begin indexing at position Zero
+*  */
 
 
 /*    ArrayList  */
@@ -28,8 +36,15 @@ import java.util.Arrays;
 
 
 
-public class Arrays () {
+public class Arrays {
 
-   public static Address[]
+   public static Address[] addContact(Address [] contactArr, Address contact) {
+
+       int newArr = nameOfContact = new Contact [newArr];
+       for(int i = 0; i < newArr; i++) {
+           System.out.println(nameOfContact[i]);
+       }
+       return nameOfContact
+   }
 
         }
