bfcbf62e2c62c6aec1f81565274b958fc6155e00
Therman116220 <thermanhaynes@gmail.com>
2022-06-16 23:28:53 -0500

    post grad notes

---
+++ src/Arrays.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+
+/*Arrays Post course  Notes*/
+/*
+* Arrays are great for code reduction
+* Arrays are a bunch of variables smashed together essentually
+* Fun Reference- Like a address book, that starts w/  "0"
+*MUST DEFINE SIZE - If size will be consistant, and will not change during run-time.
+*
+*
+*
+* */
+
+
+/*    ArrayList  */
+/*NO NEED to define size
+* Arraylist is great for less
+* */
+
+
+
+
+
+
+/*Multi-dimentional Arrays*/
+
+
+
+
+public class Arrays () {
+
+   public static Address[]
+
+        }
