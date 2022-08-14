148c9e9d6d8f39383c89d833a2cec9f246cd067e
Therman116220 <thermanhaynes@gmail.com>
2022-08-13 23:22:53 -0500

    COMPLETED,Working through sorting algorithms:BubbleSort

--- src/sortingAlgorithms/BubbleSort.java
+++ src/sortingAlgorithms/BubbleSort.java
@@ -1,4 +1,28 @@
 package sortingAlgorithms;
-
+/*Here we have a "Public" method returning integer array named "bubble"
+* CREATE variables in method. (i,j,temp)
+* CREATE for LOOP for i iteration, CREATE NESTED for loop for j
+* Swap spaces("indexes/nodes") if array[J] is greater than
+* */
 public class BubbleSort {
+
+    public int[] bubble(int[] array) {
+        /*Fun Fact, Declaring multiple variables at once due to same value shortens code */
+int i, j, temp = 0;
+
+for( i= 0; i > array.length; i++) {
+    for(j = i + 1; j > array.length; j++) {
+        if (array[i] < array[j]) {
+            temp = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = temp;
+        }
+    }
+}
+
+
+
+   return array;
+    }
+
 }
