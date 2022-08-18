2359f09d3a8ca3085ffea3c965c7d935d328a2a9
Therman116220 <thermanhaynes@gmail.com>
2022-08-17 23:46:55 -0500

    Merge Sorting Algorithm

--- src/sortingAlgorithms/MergeSort.java
+++ src/sortingAlgorithms/MergeSort.java
@@ -1,7 +1,21 @@
 package sortingAlgorithms;
 
+import java.util.Random;
+
+/*Complexity of 0(log n)
+* Faster
+* */
 public class MergeSort {
 
+    public static void main(String[] args) {
+        Random random  = new Random();
+
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.length;i++) {
+
+            numbers[i] = random.nextInt(10000);
+        }
 
 }
 
