9ab537777c308d66e8eb2b8a8fae32067241735d
Therman116220 <thermanhaynes@gmail.com>
2022-08-18 22:12:36 -0500

    Merge Sorting Algorithm

--- src/sortingAlgorithms/MergeSort.java
+++ src/sortingAlgorithms/MergeSort.java
@@ -16,6 +16,24 @@ public class MergeSort {
 
             numbers[i] = random.nextInt(10000);
         }
+        System.out.println("Before");
+        printArray(numbers);
+
+        mergeSort(numbers);
+
+        System.out.println("After:");
+        printArray(numbers);
+    }
+    private static void  mergeSort(int[] inputArray) {
+int inputLength = inputArray.length;
+    }
+    private  static void  printArray(int[] numbers) {
+for (int i = 0; i < numbers.length; i++) {
+    System.out.println(numbers[i]);
+}
+}
+    }
+
 
 }
 
