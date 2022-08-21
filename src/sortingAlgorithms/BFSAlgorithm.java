319f5a2a618d396378a4799f9b9a69ac1badc7e1
Therman116220 <thermanhaynes@gmail.com>
2022-08-20 23:29:50 -0500

    BFS/DFS traversing Algorithms

--- src/sortingAlgorithms/BFSAlgorithm.java
+++ src/sortingAlgorithms/BFSAlgorithm.java
@@ -6,10 +6,30 @@ package sortingAlgorithms;
     *BFS (A traversing algorithm) traverses a tree data structure, starting from the root. Looking for a node that meets req. for task.
 
 
-     - Start traversing from a SELECTED NODE, source OR STARTING NODE and traverse the "TREE" or "GRAPH", LAYER BY LAYER.
-
+     - Start traversing from a SELECTED NODE, source OR STARTING NODE and traverse the "TREE" or "GRAPH", LAYER BY LAYER/Level By Level.
+    Level:0
+    Level:1
+    Level:2
   */
+
+import com.sun.source.tree.Tree;
+import com.sun.source.tree.TreeVisitor;
 
 public class BFSAlgorithm {
 
+    public static void main(String[] args) {
+        Tree root = new Tree() {
+            @Override
+            public Kind getKind() {
+                return null;
+            }
+
+            @Override
+            public <R, D> R accept(TreeVisitor<R, D> visitor, D data) {
+                return null;
+            }
+        };
+
+
+    }
 }
