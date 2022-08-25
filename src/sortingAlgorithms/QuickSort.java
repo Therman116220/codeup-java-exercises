34f69c5e5ae6d4888cda738edf253ea9996bf393
Therman116220 <thermanhaynes@gmail.com>
2022-08-24 23:54:30 -0500

    QuickSort

--- src/sortingAlgorithms/QuickSort.java
+++ src/sortingAlgorithms/QuickSort.java
@@ -12,10 +12,19 @@ one half has larger items.*/
 import java.util.Arrays;
 
 public class QuickSort {
-
+/*Array Creation,Calling & Print*/
     public static void main(String[] args) {
         int[] arr = {1,9,12,4,5};
         quickSort(arr);
         System.out.println(Arrays.toString(arr));
     }
+    /*Here we actually define the algorithm as recursive & Sort it*/
+    public static void quickSort(int[] array) {
+        quickSortBase(array,0,array.length - 1);
+    }
+    /*Here we set the Base Case(needs no sorting) for the Sorting Algorithm*/
+    public static void quickSortBase(int[] array, int begin, int end) {
+
+    }
+
 }
