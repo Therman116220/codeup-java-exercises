0f83c9e5c7e56fddb457a1c17faf5abbdf7e06fa
Therman116220 <thermanhaynes@gmail.com>
2022-08-26 23:45:13 -0500

    redo/cleaning code

--- src/sortingAlgorithms/FizzBuzz.java
+++ src/sortingAlgorithms/FizzBuzz.java
@@ -35,16 +35,3 @@ class Result {
 return String.ValueOf(n);
 }
 
-
-
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
-
-        Result.fizzBuzz(n);
-
-        bufferedReader.close();
-    }
-}
