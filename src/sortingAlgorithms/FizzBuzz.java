df5f09bd7327f41f65f60cb7a47e2bdd48ebfa48
Therman116220 <thermanhaynes@gmail.com>
2022-08-26 23:58:08 -0500

    redo/cleaning code

--- src/sortingAlgorithms/FizzBuzz.java
+++ src/sortingAlgorithms/FizzBuzz.java
@@ -1,37 +1,21 @@
 package sortingAlgorithms;
 
 
-import java.io.*;
-        import java.math.*;
-        import java.security.*;
-        import java.text.*;
-        import java.util.*;
-        import java.util.concurrent.*;
-        import java.util.function.*;
-        import java.util.regex.*;
-        import java.util.stream.*;
-        import static java.util.stream.Collectors.joining;
-        import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.joining;
 
 
-
-class Result {
-
+class FizzBuzz {
     /*
-     * Complete the 'fizzBuzz' function below.
-     *
-     * The function accepts INTEGER n as parameter.
+    * Declare static method
+     -The function accepts a 'int' we will name 'nums' as parameter.
+     * CFS(Control Flow Statement)- if/then
      */
 
-    public static String fizzBuzz(int n) {
-        if (n % 3 == 0) {
-            return "fizzBuzz";
-        } else {
-            return "fizz";
-        }
-    } else if (number % 5 == 0) {
-        return "buzz";
-    }
-return String.ValueOf(n);
-}
+    public static void main(int nums) {
 
+
+
+
+
+        }
+    }
