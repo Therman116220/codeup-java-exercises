bea02a22a570eb108735ba542cc25aa76d5d2a64
Therman116220 <thermanhaynes@gmail.com>
2022-08-27 23:23:38 -0500

    BFS

--- src/sortingAlgorithms/BFSAlgorithm.java
+++ src/sortingAlgorithms/BFSAlgorithm.java
@@ -4,9 +4,7 @@ package sortingAlgorithms;
     *Begins at TREE root
     *Uses "Queue"
     *BFS (A traversing algorithm) traverses a tree data structure, starting from the root. Looking for a node that meets req. for task.
-
-
-     - Start traversing from a SELECTED NODE, source OR STARTING NODE and traverse the "TREE" or "GRAPH", LAYER BY LAYER/Level By Level.
+    - Start traversing from a SELECTED NODE, source OR STARTING NODE and traverse the "TREE" or "GRAPH", LAYER BY LAYER/Level By Level.
     Level:0
     Level:1
     Level:2
@@ -16,7 +14,23 @@ import com.sun.source.tree.Tree;
 
 public class BFSAlgorithm {
 
-    public static void main(String[] args) {
+
+}
+
+
+
+
+
+/*
+* Explore nodes and edges of graph.Often a building block in other algorithms.
+* Time Commplexity- O(V+E)
+* Basic Use- Finding 'shortest path on unweighted graphs.
+*
+*
+* */
+
+
+/*    public static void main(String[] args) {
         Tree root = new Tree(10);
         Tree newChild = root.addChild(5); //roots first child
         root.addChold(7);
@@ -31,5 +45,6 @@ public class BFSAlgorithm {
         } else {
             System.out.println("Nothing found here!");
         }
-    }
-}
+    }*/
+
+
