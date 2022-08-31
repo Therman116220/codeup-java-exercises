21abffc62bcfafef5027daf85da8334dfd2ca2c0
Therman116220 <thermanhaynes@gmail.com>
2022-08-30 23:36:25 -0500

    Update

---
+++ src/sortingAlgorithms/Test.java
@@ -0,0 +1,59 @@
+package sortingAlgorithms;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Test {
+
+    /* Create Grade Checker
+    *  Variables: student, grade, passed, failed, class[], total, classAverage
+    *
+    *
+    *
+    *
+    * */ /*? Can I recreate with a queue?   T/S Complexity? */
+
+
+    abstract class Man{
+    abstract void  move();
+    abstract void run();
+    abstract void jump();
+    abstract void fight();
+
+    // Consists of attack(),superAttack(),defend(),potion(),runaway().
+
+        //MOVE     Consists of obvious R(),L(),F(),B()      -OUT OF COMBAT
+        //RUN      Will be used w/ both move() & fight()-'SPECIFICALLY fight(runaway());  --runaway() should implement run()
+    }
+    class Person extends Man {
+
+      @Overide
+        void  move();
+
+      @Overide
+        void move();
+
+
+    }
+
+
+
+
+
+
+
+
+   /* Queue<String> queue = new LinkedList<>(String)();
+*/
+
+
+
+
+
+
+
+
+
+
+
+}
