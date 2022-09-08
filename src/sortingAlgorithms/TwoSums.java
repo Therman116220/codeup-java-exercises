3100f1b60476e9d2d7be17a2ee4bc2ef61bf26b2
Therman116220 <thermanhaynes@gmail.com>
2022-09-08 00:34:15 -0500

    TwoSums

--- src/sortingAlgorithms/TwoSums.java
+++ src/sortingAlgorithms/TwoSums.java
@@ -2,15 +2,16 @@ package sortingAlgorithms;
 
 class TwoSums {
 
-
     public int[] twoSum(int[] nums, int target) {
 
-        int i;
-        for (i = 0; i < nums.length; i++) {
+
+        for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                if (i + j == target) {
-                    System.out.println(i + "plus" + j + "equals" + target);
-                }
+               int k = target - nums[i];
+
+               if(nums[j] == k) {
+                   return  new int[] {i,j};
+               }
             }
         }
         return nums;
