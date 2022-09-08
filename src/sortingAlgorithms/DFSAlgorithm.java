9efd5780b31624aae5ae95ea128c7cc580167cc2
Therman116220 <thermanhaynes@gmail.com>
2022-09-07 23:06:34 -0500

    DFS

--- src/sortingAlgorithms/DFSAlgorithm.java
+++ src/sortingAlgorithms/DFSAlgorithm.java
@@ -3,5 +3,25 @@ package sortingAlgorithms;
 //DFS-> uses stack or recursion (less code THAN BFS) VS
 
 
+import static jdk.internal.org.jline.utils.Colors.s;
+
 public class DFSAlgorithm {
+
+    graph = {
+            'A' : ['B','G'],
+
+    }
+
+ public static method (graph, node) {
+     visited = [];
+     stack = deque();
+
+     visitied.append();
+     stack.append();
+
+     /*Control Flow Statement  - While Loop/ For Loop*/
+
+
+ }
+
 }
