ff4c0979a01ce8145a01c5f8b6c99072ea8d2be4
Therman116220 <thermanhaynes@gmail.com>
2022-09-10 23:11:04 -0500

    Queues

--- src/sortingAlgorithms/Arrays.java
+++ src/sortingAlgorithms/Arrays.java
@@ -7,13 +7,6 @@ class  Arrays {
     public static void main(String[] args) {
 
 
-    }
-    public int[] arrayPracice(int[] nums, int target) {
-        for(int i = 0; i< nums.length; i++) {
-
-        }
-        return nums;
-    }
 
 
 
@@ -31,4 +24,5 @@ class  Arrays {
 
 
 
-}
+
+}}
--- src/sortingAlgorithms/Stacks.java
+++ src/sortingAlgorithms/Stacks.java
@@ -1,9 +1,30 @@
 package sortingAlgorithms;
 import java.util.Stack;
 
-
+//FILO
 public class Stacks {
+
+
     public static void main(String[] args) {
+        Stack<Integer> nums = new Stack<>();
+
+        nums.add(5);
+        nums.add(9);
+        nums.add(10);
+        nums.add(11);
+        System.out.println(nums.peek());  //11
+        nums.pop(); //10
+        System.out.println(nums.peek());
+        nums.pop();//9
+        System.out.println(nums.peek());
+        nums.pop();//5
+        System.out.println(nums.peek());
+        nums.pop();
+        System.out.println(nums.empty());//true
+
+
+    }
+ /*   public static void main(String[] args) {
 
         Stack<String> movie = new Stack<>(); //replaced Stack<String> w/ <> for simplicity
         movie.add("Avatar");
@@ -15,5 +36,5 @@ public class Stacks {
         System.out.println("Popped: "+ movie.pop());
         System.out.println("Popped: "+ movie.pop());
         System.out.println("Empty: "+movie.empty());
-    }
+    }*/
 }
