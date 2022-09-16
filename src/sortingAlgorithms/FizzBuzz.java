4a70c7a10c48a4914babda9e0db1fa90bcc6fc06
Therman116220 <thermanhaynes@gmail.com>
2022-09-16 09:42:48 -0500

    Fixed SCO(Short Circuit operator)bug in algorithm

--- src/sortingAlgorithms/FizzBuzz.java
+++ src/sortingAlgorithms/FizzBuzz.java
@@ -15,7 +15,7 @@ class FizzBuzz {
 
         for (int i=1; i<=nums; i++) {
             /*is divisible by both 3 & 5*/
-            if (i%5==0 || i%3==0) {
+            if (i%5==0 && i%3==0) {
                 System.out.println("FizzBuzz");
             } else if (i%3==0) {
                 System.out.println("Fizz");
