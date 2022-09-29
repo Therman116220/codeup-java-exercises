1443c1e70470d3f417b72394a5b654a28671adeb
Therman116220 <thermanhaynes@gmail.com>
2022-09-28 23:29:06 -0500

    Update

--- src/practiceWithAlgorithms/ReverseSort.java
+++ src/practiceWithAlgorithms/ReverseSort.java
@@ -2,16 +2,31 @@ package practiceWithAlgorithms;
 
 public class ReverseSort {
     public static void main(String[] args) {
+      String r = reverse("Goat");
+        System.out.println(r);
+    }
+    public static void reverse(String s) {
+        char[] node = new char[s.length()];
+        //We want "i" to be the length of our argument - 1 *the last node*
+        //If "i", *currently placed at the last node*
+        // is greater than or equal to 0 decrement "i"
+        int nodeIndex = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            node[nodeIndex] = s.charAt(i);
+            nodeIndex++;
+        }
+
+    }
+    /*1*/
+  /*  public static void main(String[] args) {
         String r = reverse("senyah");
         System.out.println(r);
-
     }
 
     public static String reverse(String s) {
         char[] node = new char[s.length()];
 
-        int nodeIndex = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
+        int nodeIndex = 0;for (int i = s.length() - 1; i >= 0; i--) {
             node[nodeIndex] = s.charAt(i);
             nodeIndex++;
         }
@@ -22,9 +37,9 @@ public class ReverseSort {
         }
         return reverseContent.toString();
         //  Alternate way to do LINE 19 - 23
-       /* String reverse = "";
+       *//* String reverse = "";
         for (int i = 0; i < s.length(); i++) {
-            reverseContent = reverseContent + letters[i];*/
+            reverseContent = reverseContent + letters[i];*//*
         // *** With a return statement of 'reverseContent'  ***
-    }
+    }*/
 }
