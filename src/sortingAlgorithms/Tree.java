4d5dc6ea784c30fd22141349d5f9af6cd0f4efc2
Therman116220 <thermanhaynes@gmail.com>
2022-10-02 22:59:01 -0500

    Tree

--- src/sortingAlgorithms/Tree.java
+++ src/sortingAlgorithms/Tree.java
@@ -19,10 +19,49 @@ import java.util.List;
 public class Tree {
 
     /*2*/
+  //  Create-
+    //  class
+    //  variables: L & R
+    //  constructor(int data)
+    //  Binary Tree
+
+private TreeNode root;
+
+private class TreeNode {
+
+    private TreeNode left;
+    private TreeNode right;
+    private int data;
+
+    public TreeNode( int data) {
+        this.data = data;
+    }
+}
+//populating node data node by node
+    public void binaryTree() {
+    TreeNode first = new TreeNode(1);
+    TreeNode second = new TreeNode(2);
+    TreeNode third = new TreeNode(3);
+    TreeNode four = new TreeNode(4);
+    TreeNode five = new TreeNode(5);
+    TreeNode six = new TreeNode(6);
+    TreeNode seven = new TreeNode(7);
+
+    // re-assign our node value(1) to root
+    root = first;
+    first.left = second;
+    first.right = third;
+
+    second.left = third;
+    second.right = four;
+
+}
 
 
 
-    //Tree<String> root = new Tree<>("root");
+    public static void main(String[] args) {
+
+    }
 
 
 
@@ -64,7 +103,7 @@ public  Tree addChild(int value) {
 
 */
 
-    /*Example*/
+                                                        /*Example*/
 
 
     /* int node;
@@ -89,7 +128,5 @@ public  Tree addChild(int value) {
             }
         }
     }*/
-
-
-
+    /*------------------------------------------------------------------------------------------------*/
 }
