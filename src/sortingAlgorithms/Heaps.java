a5729a9e2a83d9ff7f8f740acfd0c275f2d538cc
Therman116220 <thermanhaynes@gmail.com>
2022-10-06 14:00:56 -0500

    Reworking/renaming:Heaps

--- src/sortingAlgorithms/Heaps.java
+++ src/sortingAlgorithms/Heaps.java
@@ -5,6 +5,13 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+
+// Questions&Notes
+//What are some use cases or real world applications?
+//        *Heap Sort
+//Used for finding min & max fast.
+//What is a priority queue? How to use it as a heap?
+//        * PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 public class Heaps {
 
 
