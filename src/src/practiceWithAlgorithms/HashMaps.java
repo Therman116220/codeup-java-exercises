0b50a9145305804a0b42862b00df0ec447edeac9
Therman116220 <thermanhaynes@gmail.com>
2022-11-15 10:42:08 -0600

    hashmaps

--- src/src/practiceWithAlgorithms/HashMaps.java
+++ src/src/practiceWithAlgorithms/HashMaps.java
@@ -13,13 +13,9 @@ public class HashMaps {
         manager.put("KeyFour", 4);
 
         System.out.println(manager);
-        System.out.println("searching for value of KeyThree by isolating the Key: ");
         System.out.println(manager.containsKey("KeyThree"));
-        System.out.println("Grabbing KeyThree by isolating it's Value: ");
         System.out.println(manager.containsValue(3));
-
         System.out.println(manager);
-
         System.out.println(manager.get("Key"));
         System.out.println(manager.get("KeyTwo"));
         System.out.println(manager.replace("KeyFour", 4, 1010));
